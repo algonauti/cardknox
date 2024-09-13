20a236803acc6e58722dc09efacc3d4287f0583b
Salvatore Ferrucci <salvatore@algonauti.com>
2024-09-13 17:39:19 +0200

    setup dotenv

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 require "cardknox"
+require "dotenv/load"
+
+Dotenv.overload(".env", ".env.test")
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
@@ -9,7 +12,7 @@ RSpec.configure do |config|
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
 
-  config.expect_with :rspec do |c|
+  config.expect_with(:rspec) do |c|
     c.syntax = :expect
   end
 end
